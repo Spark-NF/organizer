fb68a0e5a7734c1eb1199e9100a3d1b4fb91dfe3
Spark-NF <spark_ps@hotmail.fr>
2024-08-18 00:12:02 +0200

    tests: add test for the process action

---
+++ src/lib/tests/actions/process-action-test.cpp
@@ -0,0 +1,31 @@
+#include "actions/process-action.h"
+#include <QFile>
+#include <catch.h>
+
+
+TEST_CASE("ProcessAction")
+{
+	SECTION("Not found")
+	{
+		ProcessAction action("this_program_does_not_exist", {});
+
+		QFile file("test.jpg");
+		REQUIRE(action.execute(file) == false);
+	}
+
+	SECTION("Execute")
+	{
+		#if defined(Q_OS_WINDOWS)
+			const QString command = "cmd";
+			const QStringList args { "/C", "echo test" };
+		#else
+			const QString command = "sh";
+			const QStringList args { "-c", "echo test" };
+		#endif
+
+		ProcessAction action(command, args);
+
+		QFile file("test.jpg");
+		REQUIRE(action.execute(file) == true);
+	}
+}
