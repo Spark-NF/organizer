edf90d93d6e91eb9c39813dcdbc0a122d0fd2645
Spark-NF <spark_ps@hotmail.fr>
2024-08-18 00:03:24 +0200

    tests: add test ProfileLoader::load

--- src/lib/tests/profile-loader-test.cpp
+++ src/lib/tests/profile-loader-test.cpp
@@ -1,35 +1,35 @@
 #include "profile-loader.h"
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QTemporaryFile>
 #include "profile.h"
 #include "rules/rule.h"
-#include <QTemporaryFile>
 #include <catch.h>
-
-
-const QString basicProfile = R"0(
-{
-    "name": "Test profile",
-    "rules": [
-        {
-            "name": "Test rule",
-            "shortcut": "A",
-			"conditions": [{
-				"type": "filename",
-				"filename": "match",
-				"regex": true
-			}],
-            "actions": [{
-				"type": "rename",
-				"from": "(.+)",
-				"to": "before_\\1"
-			}]
-		}
-	]
-}
-)0";
 
 
 TEST_CASE("ProfileLoader")
 {
+	QJsonObject jsonProfile {
+		{ "name", "Test profile" },
+		{ "rules", QJsonArray {
+			QJsonObject {
+				{ "name", "Test rule" },
+				{ "shortcut", "A" },
+				{ "conditions", QJsonArray { QJsonObject {
+					{ "type", "filename" },
+					{ "filename", "match" },
+					{ "regex", true },
+				}}},
+				{ "actions", QJsonArray { QJsonObject {
+					{ "type", "rename" },
+					{ "from", "(.+)" },
+					{ "to", "before_\\1" },
+				}}},
+			}
+		}}
+	};
+
 	SECTION("Load file")
 	{
 		SECTION("File not found")
@@ -53,10 +53,69 @@ TEST_CASE("ProfileLoader")
 		{
 			QTemporaryFile file;
 			file.open();
-			file.write(basicProfile.toUtf8());
+			file.write(QJsonDocument(jsonProfile).toJson());
 			file.close();
 
 			QSharedPointer<Profile> profile = ProfileLoader::loadFile(file.fileName());
+			REQUIRE(profile != nullptr);
+
+			REQUIRE(profile->name() == "Test profile");
+			REQUIRE(profile->rules().count() == 1);
+			REQUIRE(profile->rules()[0][0]->name() == "Test rule");
+		}
+	}
+
+	SECTION("Load")
+	{
+		SECTION("Valid")
+		{
+			QSharedPointer<Profile> profile = ProfileLoader::load(jsonProfile);
+			REQUIRE(profile != nullptr);
+
+			REQUIRE(profile->name() == "Test profile");
+			REQUIRE(profile->rules().count() == 1);
+			REQUIRE(profile->rules()[0][0]->name() == "Test rule");
+		}
+
+		SECTION("Ignore invalid rules")
+		{
+			jsonProfile["rules"].toArray().append(QJsonObject {
+				QJsonObject {
+					{ "name", "Another rule" },
+					{ "shortcut", "B" },
+					{ "conditions", QJsonArray() },
+					{ "actions", QJsonArray() },
+				}
+			});
+
+			QSharedPointer<Profile> profile = ProfileLoader::load(jsonProfile);
+			REQUIRE(profile != nullptr);
+
+			REQUIRE(profile->name() == "Test profile");
+			REQUIRE(profile->rules().count() == 1);
+			REQUIRE(profile->rules()[0][0]->name() == "Test rule");
+		}
+
+		SECTION("Duplicate shortcut")
+		{
+			jsonProfile["rules"].toArray().append(QJsonObject {
+				QJsonObject {
+					{ "name", "Another rule" },
+					{ "shortcut", "A" },
+					{ "conditions", QJsonArray { QJsonObject {
+						{ "type", "filename" },
+						{ "filename", "match" },
+						{ "regex", true },
+					}}},
+					{ "actions", QJsonArray { QJsonObject {
+						 { "type", "rename" },
+						 { "from", "(.+)" },
+						 { "to", "after_\\1" },
+					 }}},
+				}
+			});
+
+			QSharedPointer<Profile> profile = ProfileLoader::load(jsonProfile);
 			REQUIRE(profile != nullptr);
 
 			REQUIRE(profile->name() == "Test profile");
