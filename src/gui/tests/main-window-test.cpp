cb3f3f8b69fafb026efb348f3dcf81747b1902e5
Spark-NF <spark_ps@hotmail.fr>
2025-09-07 18:03:53 +0200

    test: add test for main window buttons

--- src/gui/tests/main-window-test.cpp
+++ src/gui/tests/main-window-test.cpp
@@ -1,9 +1,13 @@
 #include <catch.h>
 #include <QApplication>
 #include <QFile>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QMenuBar>
 #include <QMessageBox>
 #include <QPushButton>
 #include <QSignalSpy>
+#include <QTemporaryFile>
 #include <QTest>
 #include <QTimer>
 #include "main-window.h"
@@ -44,6 +48,63 @@ TEST_CASE("Main window")
 
 					mainWindow->close();
 				});
+			});
+
+			MainWindow mainWindow;
+			QSignalSpy spy(&mainWindow, &MainWindow::closed);
+			mainWindow.show();
+			spy.wait();
+		}
+	}
+
+	SECTION("Automatically load action file if saved in settings")
+	{
+		const QJsonObject jsonProfile {
+			{ "name", "Test profile" },
+			{ "rules", QJsonArray {
+				QJsonObject {
+					{ "name", "Before" },
+					{ "shortcut", "B" },
+					{ "actions", QJsonArray { QJsonObject {
+						{ "type", "rename" },
+						{ "from", "(.+)" },
+						{ "to", "before_\\1" },
+					}}},
+				},
+				QJsonObject {
+					{ "name", "After" },
+					{ "shortcut", "A" },
+					{ "actions", QJsonArray { QJsonObject {
+						{ "type", "rename" },
+						{ "from", "(.+)" },
+						{ "to", "\\1_after" },
+					}}},
+				}
+			}}
+		};
+		QTemporaryFile file;
+		file.open();
+		file.write(QJsonDocument(jsonProfile).toJson());
+		file.close();
+
+		QSettings settings("settings.ini", QSettings::IniFormat);
+		settings.setValue("LastActionsFile", file.fileName());
+
+		SECTION("Show action buttons")
+		{
+			QTimer::singleShot(WAIT, []() {
+				// The active window should directly be the main window
+				auto *activeWindow = qApp->activeWindow();
+				auto *mainWindow = qobject_cast<MainWindow*>(activeWindow);
+				REQUIRE(mainWindow != nullptr);
+
+				// There should be only two buttons: "Previous" and "Next"
+				auto buttons = mainWindow->findChildren<QPushButton*>();
+				REQUIRE(buttons.count() == 4);
+				REQUIRE(buttons[2]->text().contains("Before"));
+				REQUIRE(buttons[3]->text().contains("After"));
+
+				mainWindow->close();
 			});
 
 			MainWindow mainWindow;
