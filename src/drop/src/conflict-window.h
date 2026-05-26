92a19d41cc29192aea27671dda18e597736b5e8f
Spark-NF <spark_ps@hotmail.fr>
2026-05-26 18:21:30 +0200

    refactor: replace Q_NULLPTR with nullptr

--- src/drop/src/conflict-window.h
+++ src/drop/src/conflict-window.h
@@ -21,7 +21,7 @@ class ConflictWindow : public QDialog
 	Q_DISABLE_COPY_MOVE(ConflictWindow)
 
 	public:
-		explicit ConflictWindow(const Media &media, const QList<std::shared_ptr<Rule>> &rules, QWidget *parent = Q_NULLPTR);
+		explicit ConflictWindow(const Media &media, const QList<std::shared_ptr<Rule>> &rules, QWidget *parent = nullptr);
 		~ConflictWindow();
 		void closeEvent(QCloseEvent *event) override;
 
