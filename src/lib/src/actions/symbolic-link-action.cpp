af8a7db8afb2ae3d12a31608d92044e47cf0fefb
Spark-NF <spark_ps@hotmail.fr>
2026-05-26 12:18:22 +0200

    fix: use correct error in SymbolicLinkAction error message

--- src/lib/src/actions/symbolic-link-action.cpp
+++ src/lib/src/actions/symbolic-link-action.cpp
@@ -24,7 +24,7 @@ bool SymbolicLinkAction::execute(Media &media) const
 	#if defined(Q_OS_WINDOWS)
 		const bool ok = CreateSymbolicLinkW(m_name.toStdWString().c_str(), media.path().toStdWString().c_str(), SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE);
 		if (!ok) {
-			qCritical() << "Error creating hard link" << GetLastError();
+			qCritical() << "Error creating symbolic link" << GetLastError();
 		}
 		return ok;
 	#else
