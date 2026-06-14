b78ee4e1ec03b3b5e3bc897f8ba8f5b605692615
Spark-NF <spark_ps@hotmail.fr>
2026-06-14 22:27:10 +0200

    fix: empty filter not working for dirs

--- src/lib/src/conditions/loaders/empty-loader.cpp
+++ src/lib/src/conditions/loaders/empty-loader.cpp
@@ -8,6 +8,6 @@ QVariant EmptyLoader::load(Media &media, const QStringList &fields) const
 {
 	const QFileInfo &info = media.fileInfo();
 	if (info.isDir())
-		return QDir(info.absoluteFilePath()).isEmpty(QDir::NoDotAndDotDot);
+		return QDir(info.absoluteFilePath()).isEmpty();
 	return info.size() == 0;
 }
